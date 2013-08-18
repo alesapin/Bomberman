a10a40de0498b55b3eefb0c3f5469bb5039b8d96
alesapin <alesapin@gmail.com>
2013-08-18 16:11:17 +0400

    Fibbonchi

---
+++ new/fib.java
@@ -0,0 +1,10 @@
+public class fib{
+    public static int fib(int n){
+        if(n==1) return 1;
+        if(n==2) return 1;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args){
+        System.out.println(fib(5));
+    }
+}
